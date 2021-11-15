a55e5f3dfd5876bb7151c737d2b3e5162aaf4b26
herculesdevbr <herculessantos306@gmail.com>
2021-11-15 01:35:48 -0300

    inclusão de dados array pokeapi

--- src/App.js
+++ src/App.js
@@ -7,15 +7,28 @@ function App() {
 
   const getAllPokemons = async () => {
     const res = await fetch(loadMore)
-    const data = res.json()
+    const data = await res.json()
 
-    console.log(data)
+    setLoadMore(data.next)
+
+    function createPokemonObject  (result){
+        result.forEach(async (pokemon) => {
+            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+            const data = res.json()
+
+          setAllPokemons(currentList => [...currentList, data])
+          
+        })
+        
+    }
+    createPokemonObject(data.results)
+    await console.log(allPokemons)
   }
 
   useEffect(() => {
     getAllPokemons()
   }, [])
-  
+
   return (
     <div className="app-container">
      <h1>Pokemon Evolution</h1>
